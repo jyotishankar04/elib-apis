25796dbc6762f3fc7566898d78910892a0fbc780
jyotishankar04 <patrajyotishankar@gamil.com>
2024-06-19 22:02:10 +0530

    setting enverinment variable

--- server.ts
+++ server.ts
@@ -1,7 +1,8 @@
+import { config } from "./src/config/config";
 import app from "./src/app";
 
 const startServer = () => {
-  const port = process.env.PORT || 3000;
+  const port = config.port || 3000;
   app.listen(port, () => {
     console.log(`Listening on port number: ${port}`);
   });
---
+++ src/config/config.ts
@@ -0,0 +1,7 @@
+import { config as conf } from "dotenv";
+conf();
+const _config = {
+  port: process.env.PORT,
+};
+
+export const config = Object.freeze(_config);
