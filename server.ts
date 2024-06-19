0013f83fcbc2edc7418017c2f9d47b40a23d1e30
jyotishankar04 <patrajyotishankar@gamil.com>
2024-06-19 21:55:40 +0530

    init

---
+++ server.ts
@@ -0,0 +1,9 @@
+import app from "./src/app";
+
+const startServer = () => {
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`Listening on port number: ${port}`);
+  });
+};
+startServer();
