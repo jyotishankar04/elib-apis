eba44807f0017496fb519bf2b0ab703d508123ed
jyotishankar04 <patrajyotishankar@gamil.com>
2024-06-20 14:19:29 +0530

    add user model

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { signUpValidate } from "../utils/zod";
 import createHttpError from "http-errors";
+import User from "./userDataModel";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
@@ -11,7 +12,12 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     const error = createHttpError(400, "All fields are required");
     return next(error);
   }
-  // Process
+  //check if the user exists
+  const isExists = await User.findOne({ email });
+  if (isExists) {
+    const error = createHttpError(400, "user already exist with this email");
+    return next(error);
+  }
 
   // Responce
   res.json({ message: "User created" });
---
+++ src/user/userDataModel.ts
@@ -0,0 +1,23 @@
+import mongoose, { model } from "mongoose";
+import { User } from "./userTypes";
+
+const userSchema = new mongoose.Schema<User>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model<User>("User", userSchema);
